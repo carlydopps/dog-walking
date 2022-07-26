d5871f0a6eade39c8203e053ba16966fcc5b52d0
Carly Doppelheuer <carly.doppelheuer@gmail.com>
2022-07-26 15:25:22 -0600

    Adding Who is Walking Whom feature

--- scripts/Assignments.js
+++ scripts/Assignments.js
@@ -1,12 +1,14 @@
-import { getPets, getWalkers } from "./database.js"
+import { getPets, getWalkers, getWalkerCities, getCities } from "./database.js"
 
 // Get copy of state for use in this module
 const pets = getPets()
 const walkers = getWalkers()
+const walkerCities = getWalkerCities()
+const cities = getCities()
 
 
 // Function whose responsibility is to find the walker assigned to a pet
-const findWalker = (pet, allWalkers) => {
+export const findWalker = (pet, allWalkers) => {
     let petWalker = null
 
     for (const walker of allWalkers) {
@@ -18,16 +20,49 @@ const findWalker = (pet, allWalkers) => {
     return petWalker
 }
 
+const findCity = (walker, allWalkerCities) => {
+    let currentCity = null
+
+
+    // foundMatch will have the value of match object that made line 31 evaluate to true
+    const foundMatch = walkerCities.find(
+        // Anynomous funciton / callback function
+        (match) => {
+            return match.walkerId === walker.id
+        }
+    )
+
+    // foundCity will have the value of the city object that made line 38 evaluate to true
+    const foundCity = cities.find(
+        (city) => {
+            return foundMatch.cityId === city.id
+        }
+    )
+
+
+    // for (const match of walkerCities) {
+    //     if (match.walkerId === walker.id) {
+    //         for (const city of cities) {
+    //             if (match.cityId === city.id) {
+    //                 currentCity = city
+    //             }
+    //         }
+    //     }
+    // }
+    return foundCity
+}
+
 export const Assignments = () => {
     let assignmentHTML = ""
     assignmentHTML += "<ul>"
 
     for (const currentPet of pets) {
         const currentPetWalker = findWalker(currentPet, walkers)
+        const currentCity = findCity(currentPetWalker, walkerCities)
         assignmentHTML += `
             <li>
                 ${currentPet.name} is being walked by
-                ${currentPetWalker.name} in ${currentPetWalker.city}
+                ${currentPetWalker.name} in ${currentCity.name}
             </li>
         `
     }
@@ -36,4 +71,9 @@ export const Assignments = () => {
 
     return assignmentHTML
 }
+
+
+// Find array method iterates through and as soon as the quality check is true, it ends so it returns first match
+
+// For loops never stop, so it returns the last match
 
--- scripts/RegisteredPets.js
+++ scripts/RegisteredPets.js
@@ -1,12 +1,14 @@
-import { getPets } from "./database.js"
+import { getPets, getWalkers } from "./database.js"
+import { findWalker } from "./Assignments.js"
 
 const pets = getPets()
+const walkers = getWalkers()
 
 export const RegisteredPets = () => {
     let petHTML = "<ul>"
 
     for (const pet of pets) {
-        petHTML += `<li>${pet.name}</li>`
+        petHTML += `<li id="pet--${pet.id}">${pet.name}</li>`
     }
 
     petHTML += "</ul>"
@@ -14,3 +16,36 @@ export const RegisteredPets = () => {
     return petHTML
 }
 
+// Define an event listener to display a message when a pet is clicked
+    // Define a variable to store the value of the target
+    // If the primary key of the target starts with "pet"
+        // Split the id and use array deconstruction to store the integer in a new variable
+    // For each pet in the pets array
+        // If the pet id matches the target id after applying parse Int
+            // Generate a message saying "[Pet name] is being walked by [walker name]"
+
+document.addEventListener (
+    "click",
+    (clickEvent) => {
+        if (clickEvent.target.id.startsWith("pet")) {
+            const [, petId] = clickEvent.target.id.split("--")
+            // for (const pet of pets) {
+            //     if (pet.id === parseInt(petId)) {
+            //         // const assignedWalker = findWalker(pet, walkers)
+            //         for (const walker of walkers) {
+            //             if (walker.id === pet.walkerId) {
+            //                 window.alert(`${pet.name} is being walked by ${walker.name}`)
+            //             }
+            //         }
+            //     }
+            // }
+            const foundPet = pets.find(
+                (pet) => {
+                    return pet.id === parseInt(petId)
+                }
+            )
+            const foundWalker = findWalker(foundPet, walkers)
+            window.alert(`${foundPet.name} is being walked by ${foundWalker.name}`)
+        }
+    }
+)
